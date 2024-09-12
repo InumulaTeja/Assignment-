38093c5b01732cb5a40c0b3e56e7d3b03dc868df
InumulaTeja <tejainumula05@gmail.com>
2024-09-12 15:08:24 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int n;
+ printf("enter the value of n");
+ scanf("%d",&n);
+ for(int i=1;i<=n;i++)
+{
+    
+    printf(" %d ",i*i);
+} 
+return 0;
+}
