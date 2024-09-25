6b8147fcc27806d178557b95feef6ede52ef6327
InumulaTeja <tejainumula05@gmail.com>
2024-09-25 21:56:25 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            switch(j)
+            {
+                case 1:printf("A");
+                       break;
+                case 2:printf("B");
+                       break;
+                case 3:printf("C");        break;
+                case 4:printf("D");
+                       break;
+                case 5:printf("E");        break;
+            }
+        }
+        printf("\n");
+    }
+}
