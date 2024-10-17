6ada8b6fa0b4f935732d63ad2e2d165a017db3df
InumulaTeja <tejainumula05@gmail.com>
2024-10-17 11:13:20 +0530

    Update assignment 1.26.c

--- assignment 1.26.c
+++ assignment 1.26.c
@@ -1,6 +1,8 @@
 #include<stdio.h>//program to find sum of odd and even numbers between 1 and N
+
 int main(){
-int N,a=0,b=0,i;//a is sum of odd numbers and b is sum of even numbers
+
+int N,a=0,b=0,i;//b is sum of odd numbers and a is sum of even numbers
 
     printf("enter a number:");
 
@@ -8,32 +10,21 @@ int N,a=0,b=0,i;//a is sum of odd numbers and b is sum of even numbers
 
     printf("sum of all even numbers between 1 and N is:");
 
-for(i=2;i<N;i++){
-
+for(i=0;i<=N;i++)
+{
     if(i%2==1)
-
     continue;
-
     a=a+i;
-
 }
-
     printf("%d \n",a);
-
     printf("sum of all odd numbers between 1 and N is:");
 
-for(i=2;i<N;i++){
-
+for(i=0;i<=N;i++)
+{
     if(i%2==0)
-
     continue;
-
     b=b+i;
-
 }
-
     printf("%d \n",b);
-
 return 0;
-
 }
