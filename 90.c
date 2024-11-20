401bb34c18cad3652dc098cd07e9f7246c79a070
InumulaTeja <tejainumula05@gmail.com>
2024-11-20 21:15:39 +0530

    Update 90.c

--- 90.c
+++ 90.c
@@ -21,3 +21,30 @@ int main()
    printf("%d",l);
    printf("\n length of string excluding spaces:=%d",l-d);
 }
+
+
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char s1[30];
+   int l=0,d=0;
+   char *ptr;
+   ptr=s1;
+   printf("enter the string:");
+   gets(s1);
+ 
+   printf("\n length of string including spaces:");
+   for(int i=0;i<strlen(s1);i++)
+   {
+       l++;
+       if(*(ptr + i) ==' ')
+       {
+           d++;
+       }
+   }
+   printf("%d",l);
+   printf("\n length of string excluding spaces:=%d",l-d);
+}
